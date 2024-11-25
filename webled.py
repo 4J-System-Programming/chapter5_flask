71a0a566475d34601ea2b56fdee677b162957a54
a-ymst <yamashita@tokyo-ct.ac.jp>
2024-11-25 13:44:48 +0900

    debug=Falseに変更

--- webled.py
+++ webled.py
@@ -29,5 +29,5 @@ def index():
     return render_template('webled.html', state=state)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
